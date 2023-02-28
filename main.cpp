7c74dff272b416e8defd039042650581852486ca
NguyenTri251004 <126484529+NguyenTri251004@users.noreply.github.com>
2023-02-28 18:40:16 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include<Windows.h>
 using namespace std;
-void FixConsoleWindow() {
-	HWND consoleWindow = GetConsoleWindow();
-	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
-	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
-	SetWindowLong(consoleWindow, GWL_STYLE, style);
-}
-void GotoXY(int x, int y) {
-	COORD coord;
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
+
 #define BOARD_SIZE 40 // Kích thức ma trận bàn cờ
 #define LEFT 5 // Tọa độ trái màn hình bàn cờ
 #define TOP 5 // Tọa độ trên màn hình bàn cờ
