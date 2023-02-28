201e330263669007221daee32faaa060e986ea54
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-02-28 15:14:40 +0700

    Update doan.cpp

--- doan.cpp
+++ doan.cpp
@@ -13,9 +13,9 @@ void GotoXY(int x, int y) {
 	coord.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
-#define BOARD_SIZE 20 // Kích thức ma trận bàn cờ
-#define LEFT 3 // Tọa độ trái màn hình bàn cờ
-#define TOP 1 // Tọa độ trên màn hình bàn cờ
+#define BOARD_SIZE 40 // Kích thức ma trận bàn cờ
+#define LEFT 5 // Tọa độ trái màn hình bàn cờ
+#define TOP 5 // Tọa độ trên màn hình bàn cờ
 // Khai báo kiểu dữ liệu
 struct _POINT { int x, y, c; }; // x: tọa độ dòng, y: tọa độ cột, c: đánh dấu
 _POINT _A[BOARD_SIZE][BOARD_SIZE]; //Ma trận bàn cờ
@@ -35,18 +35,41 @@ void ResetData() {
 	_X = _A[0][0].x; _Y = _A[0][0].y; // Thiết lập lại tọa độ hiện hành ban đầu
 }
 void DrawBoard(int pSize) {
-	for (int i = 0; i <= pSize; i++) {
-		for (int j = 0; j <= pSize; j++) {
-			GotoXY(LEFT + 4 * i, TOP + 2 * j);
-			printf(".");
-		}
+	for (int i = 0; i <= pSize; i++)
+	{
+		GotoXY(i, 0); printf("%c", 205); 
+		Sleep(1);//Tạm dừng 1ms
 	}
+	//Sử dụng vòng lặp vẽ đường thẳng ngang phía dưới
+	for (int i = 0; i <= pSize; i++)
+	{
+		GotoXY(i, pSize/2); printf("%c", 205);
+		Sleep(1);
+	}
+	//Sử dụng vòng lặp vẽ đường thẳng dọc bên trái
+	for (int i = 0; i <= pSize/2; i++)
+	{
+		GotoXY(0, i); printf("%c", 186);
+		Sleep(1);
+	}
+	//Sử dụng vòng lặp vẽ đường thẳng dọc bên phải
+	for (int i = 0; i <= pSize/2; i++)
+	{
+		GotoXY(pSize, i); printf("%c", 186);
+		Sleep(1);
+	}
+	// Kẻ khung bên trong
+	GotoXY(0, 0); printf("%c", 201);
+	GotoXY(0, pSize/2); printf("%c", 200);
+	GotoXY(pSize, 0); printf("%c", 187);
+	GotoXY(pSize, pSize/2); printf("%c", 188);
 }
 void StartGame() {
 	system("cls");
 	ResetData(); // Khởi tạo dữ liệu gốc
 	DrawBoard(BOARD_SIZE); // Vẽ màn hình game
 }
+
 int main()
 {
 	FixConsoleWindow();
