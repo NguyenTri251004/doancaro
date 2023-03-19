eff21981a0af1e2c0daa62b15d12cd91849be40d
HuyHuhHuh <126313050+HuyHuhHuh@users.noreply.github.com>
2023-03-19 13:28:31 +0700

    Update control.h

--- control.h
+++ control.h
@@ -4,3 +4,16 @@ void hienthi(Contro contro, Contro contro1, int daux, int dau0, Xoacontro xoacon
 void Xoacursor(Xoacontro xoacontro, Xoacontro xoacontro1, int daux, int dau0);
 void dieukhien(Contro& contro, Contro& contro1, Xoacontro& xoacontro, Xoacontro& xoacontro1, int& daux, int& dau0, XO& A, int& dem, int& dem1, int& luotchoi, int& luuct);
 void Luotchoi(int luotchoi, XO A);
+struct Toado {
+    int x, y;
+};
+struct Contro {
+    Toado td;
+};
+struct Xoacontro {
+    Toado td;
+};
+struct XO {
+    Toado vtx[300], vto[300];
+    int nx = 0, no = 0;
+};
