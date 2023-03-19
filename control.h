f0eb8a2c9f5aea1feb876a7b59129b1015fcf32e
HuyHuhHuh <126313050+HuyHuhHuh@users.noreply.github.com>
2023-03-19 13:15:51 +0700

    Update control.h

--- control.h
+++ control.h
@@ -1,1 +1,6 @@
-
+void TextColor(int x);
+void khoitao(Contro& contro, Contro& contro1);
+void hienthi(Contro contro, Contro contro1, int daux, int dau0, Xoacontro xoacontro, Xoacontro xoacontro1, XO A);
+void Xoacursor(Xoacontro xoacontro, Xoacontro xoacontro1, int daux, int dau0);
+void dieukhien(Contro& contro, Contro& contro1, Xoacontro& xoacontro, Xoacontro& xoacontro1, int& daux, int& dau0, XO& A, int& dem, int& dem1, int& luotchoi, int& luuct);
+void Luotchoi(int luotchoi, XO A);
