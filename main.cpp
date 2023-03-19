1840ede6ccefca282c9b45d86ca3d4e7237750b2
HuyHuhHuh <126313050+HuyHuhHuh@users.noreply.github.com>
2023-03-19 13:01:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,23 @@ void main() {
     setColor(1);
     FixConsoleWindow();
     DrawBoard();
+     do {
+        int daux = 0, dau0 = 0, dem = 0, dem1 = 0, luotchoi = 0, luuct = 0, xoald = 0;
+        Xoacontro xoacontro{}, xoacontro1{};
+        Contro contro, contro1;
+        XO A;
+        khoitao(contro, contro1);
+        do {
+            if (xoald != 0) Xoacursor(xoacontro, xoacontro1, daux, dau0); xoald = 1;
+            hienthi(contro, contro1, daux, dau0, xoacontro, xoacontro1, A);
+            dieukhien(contro, contro1, xoacontro, xoacontro1, daux, dau0, A, dem, dem1, luotchoi, luuct);
+            Luotchoi(luotchoi, A);
+        } while (1);
+        bool validEnter = true;
+
+    }while(1);
+    GotoXY(0, 0);
     _getch();
 }
+   
+
