ab5cce560e47f06f2698e957fcb33b8c56d945d3
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-03-23 21:30:56 +0700

    Update Control.cpp

--- Control.cpp
+++ Control.cpp
@@ -18,7 +18,21 @@ void moveUp(toado &a) {
     GotoXY(a.x, a.y-2); 
     a.y -= 2;
 }
-
+int CommandControl(toado& s, int& turn, int& countX, int& countO) {
+    GotoXY(s.x, s.y);
+    int command = _getch();
+    if ((command == 'a' || command == 'A') && s.x > 5)
+        moveLeft(s);
+    else if ((command == 'd' || command == 'D') && s.x < 71)
+        moveRight(s);
+    else if ((command == 's' || command == 'S') && s.y < 41)
+        moveDown(s);
+    else if ((command == 'w' || command == 'W') && s.y > 4)
+        moveUp(s);
+    else if (command == 32)
+        return 1;
+    return 0;
+}
     
 
 
