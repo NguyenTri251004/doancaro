5e7a5a00e45f330e929626ef614c41bf0e4c9cc8
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-03-23 13:59:33 +0700

    Update and rename control.h to Control.h

---
+++ Control.h
@@ -0,0 +1,7 @@
+struct toado {
+	int x, y;
+};
+void resetData(int matrix[73][41]);
+void CommandControl(int matrix[73][41], int& turn, int& countX, int& countO);
+int CheckThangThua(int A[73][41]);
+
