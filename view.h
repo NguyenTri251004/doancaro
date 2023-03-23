b105c0777e503a897725aac894968e2cdc06b95a
NguyenTri251004 <126484529+NguyenTri251004@users.noreply.github.com>
2023-03-23 14:15:47 +0700

    Update view.h

--- view.h
+++ view.h
@@ -2,6 +2,5 @@
 void FullScreen(); //toàn màn hình
 void FixConsoleWindow(); 
 void GotoXY(int x, int y);
-void setColor(int k);
 void DrawBoard();
 void ShowNumberTurn(int countX, int countO, int turn);
