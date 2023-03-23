752a16440f1d02e5a72f2f2a40005a1566045ac0
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-03-23 19:24:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,14 @@
 #include"Function_C.h"
 #include"Control.h"
 #include"view.h"
+#include"Game.h"
 using namespace std;
 
 void main()
 {
-    int matrix[73][41]; //-1 chua danh , 1 la cua X, 0 la cua O
-    DrawBoard();
-    resetData(matrix);
+    matrix arr[18][20];
+    DrawBoard(); //18x20 o
+    resetData(arr);
     GotoXY(33, 21);
-    int turn = 1, countX = 0, countO = 0;
-    do {
-        ShowNumberTurn(countX, countO, turn);
-        CommandControl(matrix, turn, countX, countO);
-        int check = CheckThangThua(matrix);
-        if (check == 1) {
-            GotoXY(100, 20);
-            cout << "X win";
-        }
-        else if (check == -1) {
-            GotoXY(100, 20);
-            cout << "O win";
-        }
-            
-    } while (1);
+    PvP(arr);
 }
-   
-
