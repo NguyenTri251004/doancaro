9c962e70ce36f3c92bbb4d11493b883436a77e84
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-03-23 20:59:55 +0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,6 +1,9 @@
 #pragma once
+#include"Function_C.h"
+#include"Control.h"
+#include"view.h"
 struct matrix {
 	int x, y, z;
 };
 void resetData(matrix arr[18][20]);
-void PvP(matrix arr[18][20]);
+void PvP(matrix arr[18][20], toado s);
