e7a1af75f4a74234a57be6b55ed60aab62f791ed
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-03-30 21:08:24 +0700

    Update Control.cpp

--- Control.cpp
+++ Control.cpp
@@ -2,32 +2,32 @@
 #include"view.h"
 #include "Control.h"
 //Ham move
-void moveRight(toado &a) {
+void moveRight(toado& a) {
     GotoXY(a.x + 4, a.y);
     a.x += 4;
 }
-void moveLeft(toado &a) {
-    GotoXY(a.x-4, a.y);
+void moveLeft(toado& a) {
+    GotoXY(a.x - 4, a.y);
     a.x -= 4;
 }
-void moveDown(toado &a) {
-    GotoXY(a.x, a.y+2); 
+void moveDown(toado& a) {
+    GotoXY(a.x, a.y + 2);
     a.y += 2;
 }
-void moveUp(toado &a) {
-    GotoXY(a.x, a.y-2); 
+void moveUp(toado& a) {
+    GotoXY(a.x, a.y - 2);
     a.y -= 2;
 }
 int CommandControl(toado& s) {
     GotoXY(s.x, s.y);
     int command = _getch();
-    if ((command == 'a' || command == 'A') && s.x > 5)
+    if ((command == 'a' || command == 'A') && s.x > 13)
         moveLeft(s);
-    else if ((command == 'd' || command == 'D') && s.x < 71)
+    else if ((command == 'd' || command == 'D') && s.x < 80)
         moveRight(s);
     else if ((command == 's' || command == 'S') && s.y < 41)
         moveDown(s);
-    else if ((command == 'w' || command == 'W') && s.y > 4)
+    else if ((command == 'w' || command == 'W') && s.y > 8)
         moveUp(s);
     else if (command == 32)
         return 1;
@@ -64,6 +64,4 @@ int ChooseMode(toado& mode) {
     }
     if (command == 13)
         return 1;
-}  
-
-
+}
