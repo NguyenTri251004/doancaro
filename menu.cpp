129d2a4eb7c30c8148381f01fffb74171009404a
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-03-31 17:48:22 +0700

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -24,7 +24,6 @@ void Help(int& choice)
     GotoXY(86, 34); printf("S: Len Tren");      GotoXY(110, 34); printf("8: Len Tren");
     GotoXY(86, 35); printf("W: Xuong Duoi");    GotoXY(110, 35); printf("2: Xuong Duoi");
     GotoXY(86, 36); printf("Enter: OK");            GotoXY(110, 36); printf("5: OK");
-
     GotoXY(93, 40); setColor(47, 15); printf("  Press Enter to EXIT  ");
     //GotoXY(41, 40);
     setColor(0, 15);
@@ -32,7 +31,7 @@ void Help(int& choice)
     do
     {
         a = _getch();
-    } while (a!=13);
+    } while (a != 13);
     system("cls");
     fflush(stdin);
     Menu(choice);
@@ -48,24 +47,24 @@ void About(int& choice)
     {
         GotoXY(18 + i, 21); printf(" ");
     }
-    GotoXY(94, 25);  setColor(3,15);  printf("--------ABOUT--------");
-    GotoXY(91, 28); setColor(12,15); printf("DO AN CUOI KI I - GAME CO CARO");
-    GotoXY(95, 29); setColor(12,15); printf("----- NHOM 12 -----");      GotoXY(34, 17);
+    GotoXY(94, 25);  setColor(3, 15);  printf("--------ABOUT--------");
+    GotoXY(91, 28); setColor(12, 15); printf("DO AN CUOI KI I - GAME CO CARO");
+    GotoXY(95, 29); setColor(12, 15); printf("----- NHOM 12 -----");      GotoXY(34, 17);
 
-    GotoXY(87, 31); setColor(1,15); printf("- Ho va ten -");          GotoXY(115, 31); setColor(1,15); printf("- MSSV -");
-    GotoXY(87, 32); setColor(11,15); printf("Pham Van Minh");          GotoXY(115, 32); setColor(11,15); printf("22127272");
-    GotoXY(87, 33); setColor(11,15); printf("Truong Nhat Huy");        GotoXY(115, 33); setColor(11,15); printf("22127168");
-    GotoXY(87, 34); setColor(11,15); printf("Nguyen The Quan");        GotoXY(115, 34); setColor(11,15); printf("22127341");
-    GotoXY(87, 35); setColor(11,15); printf("Nguyen Phuoc Minh Tri");  GotoXY(115, 35); setColor(11,15); printf("22127424");
+    GotoXY(87, 31); setColor(1, 15); printf("- Ho va ten -");          GotoXY(115, 31); setColor(1, 15); printf("- MSSV -");
+    GotoXY(87, 32); setColor(11, 15); printf("Pham Van Minh");          GotoXY(115, 32); setColor(11, 15); printf("22127272");
+    GotoXY(87, 33); setColor(11, 15); printf("Truong Nhat Huy");        GotoXY(115, 33); setColor(11, 15); printf("22127168");
+    GotoXY(87, 34); setColor(11, 15); printf("Nguyen The Quan");        GotoXY(115, 34); setColor(11, 15); printf("22127341");
+    GotoXY(87, 35); setColor(11, 15); printf("Nguyen Phuoc Minh Tri");  GotoXY(115, 35); setColor(11, 15); printf("22127424");
 
     GotoXY(93, 40); setColor(47, 15); printf("  Press Enter to EXIT  ");
     //GotoXY(50, 40); //chua hieu dong nay
-    setColor(0,15);
+    setColor(0, 15);
     char a;
     do
     {
         a = _getch();
-    } while (a!=13);
+    } while (a != 13);
     system("cls");
     fflush(stdin);
     Menu(choice);
@@ -161,12 +160,13 @@ void HienThiMode(toado mode) {
         GotoXY(104, 27); printf("EXIT");
         GotoXY(109, 27); printf("%c%c", 174, 174);
         setColor(4, 15);
-        
+
     }
-        
+
 }
-void Menu(int &choice) {
-	Khung();
+void Menu(int& choice) {
+    Khung();
+    HideCursor();
     toado menu = { 98,25 };
     GotoXY(98, 25);
     setColor(1, 15);
@@ -206,13 +206,13 @@ void Menu(int &choice) {
             GotoXY(menu.x + 13, menu.y + 1);
             cout << "  ";
         }
-}
+    }
     system("cls");
     if (choice == 1)
     {
         Khung();
         toado mode = { 98,25 };
-        int a ;
+        int a;
         while (1)
         {
             HienThiMode(mode);
@@ -230,22 +230,24 @@ void Menu(int &choice) {
                 break;
             }
             if (x == 0) {
-                GotoXY(mode.x+2, mode.y - 1);
+                GotoXY(mode.x + 2, mode.y - 1);
                 cout << "  ";
                 GotoXY(mode.x + 11, mode.y - 1);
                 cout << "  ";
             }
             else if (x == -1)
             {
-                GotoXY(mode.x+2, mode.y + 1);
+                GotoXY(mode.x + 2, mode.y + 1);
                 cout << "  ";
                 GotoXY(mode.x + 11, mode.y + 1);
                 cout << "  ";
             }
         }
         system("cls");
-        if (a == 1)
+        if (a == 1) {
+            ShowConsoleCursor();
             PlayGame_PvP();
+        }
         else if (a == 2)
             //PvE
             ;
@@ -260,4 +262,3 @@ void Menu(int &choice) {
     else if (choice == 3)
         About(choice);
 }
-
