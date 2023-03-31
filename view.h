b072f15989f6d98547162ea424b2616d06ddb9af
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-03-31 17:48:50 +0700

    Update view.h

--- view.h
+++ view.h
@@ -1,10 +1,11 @@
 #pragma once
 void HideCursor(); 
+void ShowConsoleCursor();
 void FullScreen();
-void setColor(int textCol, int bgCol); //update
-void FixConsoleWindow(); 
+void setColor(int textCol, int bgCol); 
+void FixConsoleWindow();
 void GotoXY(int x, int y);
-void DrawBoard(); //update
-void ShowNumberTurn(int countX, int countO, int &turn);
+void DrawBoard();
+void ShowNumberTurn(int countX, int countO, int& turn);
 void Khung(); 
 void Custom();
