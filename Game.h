04ffc7f83ea99066afb31e600c2cd96fc51cf982
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-04-16 14:24:26 +0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -2,10 +2,15 @@
 #include"Function_C.h"
 #include"Control.h"
 #include"view.h"
+#include<fstream>
+#include<string>
 #define BOARD_SIZE 18
 struct matrix {
 	int x, y, z;
 };
 void resetData(matrix arr[BOARD_SIZE][BOARD_SIZE]);
-void PlayGame_PvP();
-void PlayGame_PvE();
+int PlayGame_PvP(int&scoreX,int&scoreO);
+int PlayGame_PvE(int &score,int&scoreO);
+void SaveGame(matrix arr[BOARD_SIZE][BOARD_SIZE]);
+void LoadGameBoard(matrix arr[BOARD_SIZE][BOARD_SIZE]);
+
