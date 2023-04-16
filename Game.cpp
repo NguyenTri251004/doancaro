405625fc7018c5d991db5f0a2e5813be5b973eb5
ntq20042009 <126312599+ntq20042009@users.noreply.github.com>
2023-04-16 14:24:55 +0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,8 @@
 #include "Game.h"
 #include "control.h"
 #include "Windows.h"
-
+#include <cstring>
+const int MAX_NAMES = 5;
 using namespace std;
 //khoi tao du lieu
 void resetData(matrix arr[BOARD_SIZE][BOARD_SIZE]) {
@@ -23,73 +24,88 @@ bool checkFullBoard(matrix arr[BOARD_SIZE][BOARD_SIZE]) {
     return true;
 }
 //check win/lose/draw
-int ProcessFinish(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& a, int s)
+int ProcessFinish(matrix arr[BOARD_SIZE][BOARD_SIZE], toado a, int s)
 {
     int x = (a.y - 8) / 2;
     int y = (a.x - 13) / 4;
-    // x thang tra ve 1
-    for (int i = 0; i < 5; i++) //check hang ngang 
-    {
-        if (arr[x - 4 + i][y].z == s && arr[x - 3 + i][y].z == s && arr[x - 2 + i][y].z == s && arr[x - 1 + i][y].z == s && arr[x + i][y].z == s) {
+
+    // Check for winning conditions in the horizontal direction
+    for (int i = max(x - 4, 0); i <= min(x, BOARD_SIZE - 5); i++) {
+        if (arr[i][y].z == s && arr[i + 1][y].z == s && arr[i + 2][y].z == s && arr[i + 3][y].z == s && arr[i + 4][y].z == s) {
             return 1;
         }
     }
-    for (int i = 0; i < 5; i++) // check hang doc 
-    {
-        if (arr[x][y - 4 + i].z == s && arr[x][y - 3 + i].z == s && arr[x][y - 2 + i].z == s && arr[x][y - 1 + i].z == s && arr[x][y + i].z == s)
+
+    // Check for winning conditions in the vertical direction
+    for (int i = max(y - 4, 0); i <= min(y, BOARD_SIZE - 5); i++) {
+        if (arr[x][i].z == s && arr[x][i + 1].z == s && arr[x][i + 2].z == s && arr[x][i + 3].z == s && arr[x][i + 4].z == s) {
             return 1;
+        }
     }
-    for (int i = 0; i < 5; i++) // check duong cheo DN
-    {
-        if ((arr[x - 4 + i][y - 4 + i].z == s) && (arr[x - 3 + i][y - 3 + i].z == s) && (arr[x - 2 + i][y - 2 + i].z == s) && (arr[x - 1 + i][y - 1 + i].z == s) && (arr[x + i][y + i].z == s))
+
+    // Check for winning conditions in the diagonal direction (top-left to bottom-right)
+    for (int i = max(x - 4, 0), j = max(y - 4, 0); i <= min(x, BOARD_SIZE - 5) && j <= min(y, BOARD_SIZE - 5); i++, j++) {
+        if (arr[i][j].z == s && arr[i + 1][j + 1].z == s && arr[i + 2][j + 2].z == s && arr[i + 3][j + 3].z == s && arr[i + 4][j + 4].z == s) {
             return 1;
+        }
     }
-    for (int i = 0; i < 5; i++) // check duong cheo DB
-    {
-        if ((arr[x + 4 - i][y - 4 + i].z == s) && (arr[x + 3 - i][y - 3 + i].z == s) && (arr[x + 2 - i][y - 2 + i].z == s) && (arr[x + 1 - i][y - 1 + i].z == s) && (arr[x - i][y + i].z == s))
+
+    // Check for winning conditions in the diagonal direction (bottom-left to top-right)
+    for (int i = max(x - 4, 0), j = min(y + 4, BOARD_SIZE - 1); i <= min(x, BOARD_SIZE - 5) && j >= max(y, 4); i++, j--) {
+        if (arr[i][j].z == s && arr[i + 1][j - 1].z == s && arr[i + 2][j - 2].z == s && arr[i + 3][j - 3].z == s && arr[i + 4][j - 4].z == s) {
             return 1;
+        }
     }
-    if (checkFullBoard(arr) == true)
+
+    if (checkFullBoard(arr) == true) {
         return 0;
+    }
+
     return -1;
 }
 //in ai win/draw
-void print_Win(matrix arr[BOARD_SIZE][BOARD_SIZE], toado td, int turn) {
+void print_Win(matrix arr[BOARD_SIZE][BOARD_SIZE], toado td, int turn,int& scoreX,int&scoreO) {
     int check = ProcessFinish(arr, td, turn);
     if ((check == 1) && (turn == 1)) {
-        GotoXY(140, 25); cout << "##     ##    ##      ## #### ##    ## ";
-        GotoXY(140, 26); cout << " ##   ##     ##  ##  ##  ##  ###   ## ";
-        GotoXY(140, 27); cout << "  ## ##      ##  ##  ##  ##  ####  ## ";
-        GotoXY(140, 28); cout << "   ###       ##  ##  ##  ##  ## ## ## ";
-        GotoXY(140, 29); cout << "  ## ##      ##  ##  ##  ##  ##  #### ";
-        GotoXY(140, 30); cout << " ##   ##     ##  ##  ##  ##  ##   ### ";
-        GotoXY(140, 31); cout << "##     ##     ###  ###  #### ##    ## ";
-
+        GotoXY(91, 19); cout << "##     ##    ##      ## #### ##    ## ";
+        GotoXY(91, 20); cout << " ##   ##     ##  ##  ##  ##  ###   ## ";
+        GotoXY(91, 21); cout << "  ## ##      ##  ##  ##  ##  ####  ## ";
+        GotoXY(91, 22); cout << "   ###       ##  ##  ##  ##  ## ## ## ";
+        GotoXY(91, 23); cout << "  ## ##      ##  ##  ##  ##  ##  #### ";
+        GotoXY(91, 24); cout << " ##   ##     ##  ##  ##  ##  ##   ### ";
+        GotoXY(91, 25); cout << "##     ##     ###  ###  #### ##    ## ";
+        scoreX += 1;
     }
     else if ((check == 1) && (turn == 0)) {
-        GotoXY(140, 25); cout << " #######     ##      ## #### ##    ## ";
-        GotoXY(140, 26); cout << "##     ##    ##  ##  ##  ##  ###   ## ";
-        GotoXY(140, 27); cout << "##     ##    ##  ##  ##  ##  ####  ## ";
-        GotoXY(140, 28); cout << "##     ##    ##  ##  ##  ##  ## ## ## ";
-        GotoXY(140, 29); cout << "##     ##    ##  ##  ##  ##  ##  #### ";
-        GotoXY(140, 30); cout << "##     ##    ##  ##  ##  ##  ##   ### ";
-        GotoXY(140, 31); cout << " #######      ###  ###  #### ##    ## ";
+        GotoXY(91, 19); cout << " #######     ##      ## #### ##    ## ";
+        GotoXY(91, 20); cout << "##     ##    ##  ##  ##  ##  ###   ## ";
+        GotoXY(91, 21); cout << "##     ##    ##  ##  ##  ##  ####  ## ";
+        GotoXY(91, 22); cout << "##     ##    ##  ##  ##  ##  ## ## ## ";
+        GotoXY(91, 23); cout << "##     ##    ##  ##  ##  ##  ##  #### ";
+        GotoXY(91, 24); cout << "##     ##    ##  ##  ##  ##  ##   ### ";
+        GotoXY(91, 25); cout << " #######      ###  ###  #### ##    ## ";
+        scoreO += 1;
     }
     else if (check == 0) {
-        GotoXY(140, 25); cout << "########  ########     ###    ##      ## ";
-        GotoXY(140, 26); cout << "##     ## ##     ##   ## ##   ##  ##  ## ";
-        GotoXY(140, 27); cout << "##     ## ##     ##  ##   ##  ##  ##  ## ";
-        GotoXY(140, 28); cout << "##     ## ########  ##     ## ##  ##  ## ";
-        GotoXY(140, 29); cout << "##     ## ##   ##   ######### ##  ##  ## ";
-        GotoXY(140, 30); cout << "##     ## ##    ##  ##     ## ##  ##  ## ";
-        GotoXY(140, 31); cout << "########  ##     ## ##     ##  ###  ###  ";
-
+        GotoXY(91, 19); cout << "########  ########     ###    ##      ## ";
+        GotoXY(91, 20); cout << "##     ## ##     ##   ## ##   ##  ##  ## ";
+        GotoXY(91, 21); cout << "##     ## ##     ##  ##   ##  ##  ##  ## ";
+        GotoXY(91, 22); cout << "##     ## ########  ##     ## ##  ##  ## ";
+        GotoXY(91, 23); cout << "##     ## ##   ##   ######### ##  ##  ## ";
+        GotoXY(91, 24); cout << "##     ## ##    ##  ##     ## ##  ##  ## ";
+        GotoXY(91, 25); cout << "########  ##     ## ##     ##  ###  ###  ";
     }
 }
 //Pause khi win/draw
-int ConditionPause(matrix arr[BOARD_SIZE][BOARD_SIZE], toado td, int turn) {
-    if (ProcessFinish(arr, td, turn) == 1)
-        return 1;
+int ConditionPause(matrix arr[BOARD_SIZE][BOARD_SIZE],int turn) {
+    for (int i = 0; i < BOARD_SIZE; i++)
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            if (arr[i][j].z != 7) {
+                toado td = { arr[i][j].x,arr[i][j].y };
+                if (ProcessFinish(arr, td, turn) == 1)
+                    return 1;
+            }
+        }
     return 0;
 }
 //Dung in X/O tren man hinh
@@ -115,11 +131,11 @@ int PvP_print_X_0(matrix arr[BOARD_SIZE][BOARD_SIZE], toado s, int& turn, int& c
     }
 }
 //che do PvP
-void PvP(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s) {
+void PvP(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s,int& scoreX,int&scoreO) {
     int x = 0, turn = 1, countX = 0, countO = 0;
     while (1)
     {
-        ShowNumberTurn(countX, countO, turn);
+        ShowNumberTurn(countX, countO, turn,scoreX,scoreO);
         int command = CommandControl(s);
         if (command == 0) {
             continue;
@@ -127,10 +143,10 @@ void PvP(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s) {
         else if (command == 1)
         {
             int x = PvP_print_X_0(arr, s, turn, countX, countO);
-            if (ConditionPause(arr, s, turn) == 1) {
-                print_Win(arr, s, turn);
+            if (ConditionPause(arr, turn) == 1) {
+                system("cls");
+                print_Win(arr, s, turn,scoreX,scoreO);
                 PlaySound(L"Win", NULL, SND_ASYNC);
-                ShowNumberTurn(countX, countO, turn);
                 HideCursor();
                 break;
             }
@@ -496,7 +512,7 @@ int PvE_print_X_0(matrix arr[BOARD_SIZE][BOARD_SIZE], toado s, int& turn, int& c
         return 0;
     }
 }
-void PvE(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s) {
+void PvE(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s,int &scoreX,int&scoreO) {
     int d = 0, turn = 0, countX = 1, countO = 0;
     GotoXY(s.x, s.y);
     setColor(10, 15);
@@ -506,7 +522,7 @@ void PvE(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s) {
     arr[x][y].z = 1;
     while (1)
     {
-        ShowNumberTurn(countX, countO, turn);
+        ShowNumberTurn(countX, countO, turn,scoreX,scoreO);
         if (turn == 0) {
             int command = CommandControl(s);
             if (command == 0) {
@@ -515,10 +531,10 @@ void PvE(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s) {
             else if (command == 1)
             {
                 d = PvE_print_X_0(arr, s, turn, countO);
-                if (ConditionPause(arr, s, 0) == 1) {
-                    print_Win(arr, s, turn);
+                if (ConditionPause(arr, 0) == 1) {
+                    system("cls");
+                    print_Win(arr, s, turn,scoreX,scoreO);
                     PlaySound(L"Win", NULL, SND_ASYNC);
-                    ShowNumberTurn(countX, countO, turn);
                     HideCursor();
                     break;
                 }
@@ -536,9 +552,9 @@ void PvE(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s) {
                 kq = ProcessFinish(arr, temp, 1);
                 if (kq == 1)
                 {
-                    print_Win(arr, temp, turn);
+                    system("cls");
+                    print_Win(arr, temp, turn,scoreX,scoreO);
                     PlaySound(L"Win", NULL, SND_ASYNC);
-                    ShowNumberTurn(countX, countO, turn);
                     HideCursor();
                     break;
                 }
@@ -548,84 +564,133 @@ void PvE(matrix arr[BOARD_SIZE][BOARD_SIZE], toado& s) {
     }
 }
 //ham goi cho menu PvP
-void PlayGame_PvP() {
+int PlayGame_PvP(int& scoreX,int&scoreO) {
     matrix arr[BOARD_SIZE][BOARD_SIZE];
     toado s = { 33,22 };
     DrawBoard();
     resetData(arr);
-    PvP(arr, s);
+    PvP(arr, s,scoreX,scoreO);
+    while (1) {
+        AskContinue();
+        int command = _getch();
+        if (command == 'l' || command == 'L') {
+            system("cls");
+            ShowConsoleCursor();
+            SaveGame(arr);
+            HideCursor();
+        }
+        else if (command == 13)
+            return 1;
+        else if (command == 'T' || command == 't')
+            break;
+    }
 }
-void PlayGame_PvE() {
+void LoadGameBoard(matrix arr[BOARD_SIZE][BOARD_SIZE]) {
+    DrawBoard();
+    int countX = 0;
+    int countO = 0;
+    int scoreX = 0;
+    int scoreO = 0;
+    for (int i = 0; i < BOARD_SIZE; i++)
+        for (int j = 0; j < BOARD_SIZE; j++)
+            if (arr[i][j].z == 1) {
+                GotoXY(arr[i][j].x, arr[i][j].y);
+                setColor(10, 15);
+                cout << "X";
+                countX += 1;
+            }
+            else if (arr[i][j].z == 0) {
+                GotoXY(arr[i][j].x, arr[i][j].y);
+                setColor(12, 15);
+                cout << "O";
+                countO += 0;
+            }
+    ShowNumberTurn(countX, countO, -1, scoreX, scoreO);
+}
+int PlayGame_PvE(int&scoreX,int&scoreO) {
     matrix arr[BOARD_SIZE][BOARD_SIZE];
     toado s = { 33,22 };
     DrawBoard();
     resetData(arr);
-    PvE(arr, s);
-}
-//ham load va save game
-void LoadGame(string s)
-{
-    matrix arr[BOARD_SIZE][BOARD_SIZE];
-    resetData(arr);
-    bool load = true;
-    ifstream f1(s);
-    ifstream f;
-    if (!f1)
-    {
-        GotoXY(100, 100);
-        cout << "File ban nhap khong ton tai";
-        load = false;
-        _getch();
-    }
-    else {
-        f.open(s, ios::in);
-        for (int i = 0; i < BOARD_SIZE; i++)
-        {
-            for (int j = 0; j < BOARD_SIZE; j++)
-            {
-                f >> arr[i][j].z;
-            }
+    PvE(arr, s,scoreX,scoreO);
+    while (1) {
+        AskContinue();
+        int command = _getch();
+        if (command == 'l' || command == 'L') {
+            system("cls");
+            ShowConsoleCursor();
+            SaveGame(arr);
+            HideCursor();
         }
-        for (int i = 0; i < BOARD_SIZE; i++)
-        {
-            for (int j = 0; j < BOARD_SIZE; j++)
-            {
-                switch (arr[i][j].z)
-                {
-                case 1:
-                    GotoXY(13 + j * 4, 8 + i * 2);
-                    cout << "X";
-                    break;
-                case 0:
-                    GotoXY(13 + j * 4, 8 + i * 2);
-                    cout << "O";
-                    break;
-                case 7: cout << " ";
-                    break;
-                }
-            }
-        }
+        else if (command == 13)
+            return 1;
+        else if (command == 'T' || command == 't')
+            break;
     }
 }
-void SaveGame(matrix arr[BOARD_SIZE][BOARD_SIZE])
-{
+
+void SaveGame(matrix arr[BOARD_SIZE][BOARD_SIZE]) {
     ofstream f;
     string s;
-    GotoXY(100, 100);
-    cout << "Ten file de luu: ";
+    GotoXY(100, 27);
+    cout << "Enter name file: ";
     getline(cin, s);
+    string names[MAX_NAMES];
+    int count = 0;
+
+    ifstream file("namefile.txt");
+    string line;
+    while (getline(file, line) && count < MAX_NAMES) {
+        names[count] = line;
+        count++;
+    }
+    file.close();
+
+    bool found = false;
+    for (int i = 0; i < count; i++) {
+        if (names[i] == s) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        system("cls");
+        setColor(12, 15);
+        GotoXY(97, 27);
+        cout << "Name already exists!";
+        return;
+    }
+    else {
+        if (count == MAX_NAMES) {
+            std::remove(names[0].c_str());
+            for (int i = 0; i < MAX_NAMES - 1; i++) {
+                names[i] = names[i + 1];
+            }
+            count--;
+        }
+        names[count] = s;
+        count++;
+
+        ofstream file("namefile.txt");
+        for (int i = 0; i < count; i++) {
+            file << names[i] << endl;
+        }
+        file.close();
+    }
+
     f.open(s, ios::out);
-    for (int i = 0; i < BOARD_SIZE; i++)
-    {
-        for (int j = 0; j < BOARD_SIZE; j++)
-        {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
             GotoXY(13 + j * 4, 8 + i * 2);
             f << arr[i][j].z;
         }
         cout << endl;
     }
     f.close();
-    GotoXY(100, 100);
-    cout << "Luu thanh cong! ";
-    _getch();
+    system("cls");
+    GotoXY(97, 27);
+    setColor(12, 15);
+    cout << "Save successfully!";
 }
+
